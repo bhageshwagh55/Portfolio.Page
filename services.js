f8ca181f951dd4609ca905ad096575d674f1e911
bhageshwagh55 <bhageshwagh06@gmail.com>
2025-07-08 00:04:00 -0700

    Add files via upload

---
+++ services.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "bhagesh-portfolio-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+// Install service worker and cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate service worker
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch files from cache or network
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
